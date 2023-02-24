e11eee9f18d3bc11e4a059ce1b9b837e40c81542
Adam Horvath <horadam0207@gmail.com>
2023-02-24 22:38:20 +0100

    initial commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://adamhrvth-task-manager.netlify.app/",
   credentials: true
 }));
 
